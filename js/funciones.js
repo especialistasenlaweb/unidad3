1cc71e9b4e3208225b5c23e0b7cddc6289f3b484
Mario Rincon <jorgemariorincon@hotmail.com>
2024-11-21 16:01:59 -0500

    listo el proyecto

--- js/funciones.js
+++ js/funciones.js
@@ -1,2 +1,24 @@
-alert("hola desde Javascript");
+
+var btn=document.getElementById("btn");
+
+btn.addEventListener("click",function(){
+    var n1=document.getElementById("parcial1").value;    
+    var n2=document.getElementById("parcial2").value;    
+    var n3=document.getElementById("final").value;    
+    //alert("Nota 1: "+n1+", Nota 2:"+n2+", Nota final:"+n3);
+
+    var por1=(n1*30)/100;
+    var por2=(n2*30)/100;
+    var porf=(n3*40)/100;    
+    var nd=(por1+por2+porf).toFixed(2);
+   //alert("La nota fina es "+nd);
+   document.getElementById("mensaje").innerHTML="La nota final es "+nd;
+ 
+   if(nd<3){
+       document.getElementById("mensaje1").innerHTML="te tiraste la materia";
+    }else{
+       document.getElementById("mensaje1").innerHTML="Aprobado";
+   }
+
+});
 
